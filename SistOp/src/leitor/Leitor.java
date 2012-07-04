4c98615081a337e6ba7bf3739fccec58c7e711ac
Fernando de Andrade Pereira <fernando.an.pereira@gmail.com>
2012-07-04 03:25:00 -0300

    leitor funcionando rrrrrápido

--- SistOp/src/leitor/Leitor.java
+++ SistOp/src/leitor/Leitor.java
@@ -28,13 +28,11 @@ public class Leitor {
 		
 		linha = bf.readLine();
 		while(linha.charAt(0) == ';') linha = bf.readLine();
-		System.out.println(linha);
 		String[] lol = linha.split(" ");
 		te[0] = Integer.valueOf(lol[0]);
 		
 		linha = bf.readLine();
 		while(linha.charAt(0) == ';') linha = bf.readLine();
-		System.out.println(linha);
 		lol = linha.split(" ");
 		te[1] = Integer.valueOf(lol[0]);
 		
@@ -58,7 +56,7 @@ public class Leitor {
 				int tdp = Integer.valueOf(lol[1]);
 				int mr = Integer.valueOf(lol[2]);
 				int res = Integer.valueOf(lol[3]);
-				jobs.add(id, new Job(tdp, mr, res));
+				jobs.add(id - 1, new Job(tdp, mr, res));
 			}
 			linha = bf.readLine();
 		}
@@ -78,7 +76,7 @@ public class Leitor {
 				String[] lol = linha.split(" ");
 				int id = Integer.valueOf(lol[0]);
 				int tdc = Integer.valueOf(lol[1]);
-				tc[id] = tdc;
+				tc[id - 1] = tdc;
 			}
 			linha = bf.readLine();
 		}
