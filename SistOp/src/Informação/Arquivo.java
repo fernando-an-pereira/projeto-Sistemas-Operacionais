0250178c75d8604df545470a6e0e058db3ba0c26
manobrawl <augusto_manobrawl@hotmail.com>
2012-07-06 10:34:12 -0300

    sdfzfdg

---
+++ SistOp/src/Informação/Arquivo.java
@@ -0,0 +1,20 @@
+package Informação;
+
+
+
+public class Arquivo {
+	private boolean sendoEscrito;
+	private int tamanho;
+	private String nome;
+	
+	public Arquivo(String nome, int tamanho){
+		this.nome = nome;
+		this.tamanho = tamanho;
+		this.sendoEscrito = false;
+	}
+	
+	public boolean getSendoEscrito(){
+		return this.sendoEscrito;
+	}
+	
+}
